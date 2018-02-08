f1b747593e8a543eddbeaa78a094caea720d22e6
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-02-08 21:30:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,9 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif
-  puts "HUH?! SPEAK UP, SONNY!"
+  phrase == phrase.uppercase
+  return "NO, NOT SINCE 1938!"
+  return "HUH?! SPEAK UP, SONNY!"
 end
 end
 
